68c5cc68b0b5ea65b0dcb8311bacc1b6cd019ca0
mayanksharma468 <91319865+mayanksharma468@users.noreply.github.com>
2022-06-01 22:09:47 +0530

    Create randomrandom.py

---
+++ randomrandom.py
@@ -0,0 +1,13 @@
+# Python3 program to demonstrate
+# the use of random() function .
+
+# import random
+from random import random
+
+lst = []
+
+for i in range(10):
+    lst.append(random())
+	
+# Prints random items
+print(lst)
