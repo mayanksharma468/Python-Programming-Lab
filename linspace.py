ee150c23f5f5a942f841db67b9f43781ff661aa4
mayanksharma468 <91319865+mayanksharma468@users.noreply.github.com>
2022-06-01 22:07:23 +0530

    Create linspace.py

---
+++ linspace.py
@@ -0,0 +1,6 @@
+import numpy as np
+''' it is create array of sequential elements but but difference b/w two elements is equal to division.
+syntax-> np.linespace(start,end,division)
+NOTE-> both start and end are included'''
+a=np.linespace(1,5,10)
+print(a) 
