636598b9214fbaabd8dc8b94ea0095e8093910ac
mayanksharma468 <91319865+mayanksharma468@users.noreply.github.com>
2022-06-01 22:04:31 +0530

    Create 2D array.py

---
+++ 2D array.py
@@ -0,0 +1,7 @@
+import numpy as np  
+Student_dt = [ [72, 85, 87, 90, 69], [80, 87, 65, 89, 85], [96, 91, 70, 78, 97], [90, 93, 91, 90, 94], [57, 89, 82, 69, 60] ]  
+  
+for x in Student_dt:  
+    for i in x:    
+        sum=sum+Student_dt[i-[[0 ]]] 
+print(sum)
